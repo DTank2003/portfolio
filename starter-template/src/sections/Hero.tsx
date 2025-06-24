125906e3cf71084d6245fa7f89db4f5549995bd6
dhyey.tank <dhyey.tank@drcsystems.com>
2025-06-24 18:06:51 +0530

    Hero section with animation effects

--- starter-template/src/sections/Hero.tsx
+++ starter-template/src/sections/Hero.tsx
@@ -1,10 +1,32 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import { HeroOrbit } from "@/components/HeroOrbit";
+import StarIcon from "@/assets/icons/star.svg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div
+        className="absolute inset-0 -z-30 opacity-5"
+        style={{
+          backgroundImage: `url(${grainImage.src})`,
+        }}
+      ></div>
+      <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
+      <HeroOrbit size={800} rotation={-72}>
+        <StarIcon className="size-28 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size={550} rotation={20}>
+        <StarIcon className="size-12 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size={590} rotation={98}>
+        <StarIcon className="size-8 text-emerald-300" />
+      </HeroOrbit>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
@@ -14,22 +36,28 @@ export const HeroSection = () => {
           />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
             <div className="bg-green-500 size-2.5 rounded-full"></div>
-            <div className="text-sm font-medium">Available for new projects</div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
           </div>
         </div>
-        <h1>Building Exceptional User Experiences</h1>
-        <p>
-          I specialize in transforming designs into functional, high-performing
-          web applications. Let's discuss your next project!
-        </p>
-        <div>
-          <button>
-            <span>Explore My Work</span>
-            <ArrowDown />
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl text-center mt-8 tracking-wide md:text-5xl">
+            Building Exceptional User Experiences
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I specialize in transforming designs into functional,
+            high-performing web applications. Let's discuss your next project!
+          </p>
+        </div>
+        <div className="flex flex-col items-center mt-8 gap-4 md:flex-row justify-center">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore My Work</span>
+            <ArrowDown className="size-4" />
           </button>
-          <button>
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
             <span>👋</span>
-            <span>Let's Connect</span>
+            <span className="font-semibold ">Let's Connect</span>
           </button>
         </div>
       </div>
